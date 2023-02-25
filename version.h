feb8de7f480f3c4ccb0a37f1a0c1751f88c92a4b
Galladite <galladite@yandex.com>
2023-02-25 16:51:51 +0000

    CHANGES and version.h updated for release

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #define xstr(s) str(s)
 #define str(s) #s
 
-#define VER_NUMBER 20230220-en
+#define VER_NUMBER 20230225-en
 #define ONS_VERSION xstr(VER_NUMBER)
-#define ONS_CODENAME xstr(Gyoza)
+#define ONS_CODENAME xstr(Yakitori)
 #define NSC_VERSION 294
