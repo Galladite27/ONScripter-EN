05ba75c4cb021980ce74a6792ccc6af243baaa11
Galladite <galladite@yandex.com>
2023-04-03 18:43:22 +0100

    Minor changes to notes regarding linker flags

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #define xstr(s) str(s)
 #define str(s) #s
 
-#define VER_NUMBER 20230225-en
+#define VER_NUMBER 20230308-en
 #define ONS_VERSION xstr(VER_NUMBER)
-#define ONS_CODENAME xstr(Yakitori)
+#define ONS_CODENAME xstr(Ramen)
 #define NSC_VERSION 294
