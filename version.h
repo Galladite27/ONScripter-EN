15928cd6e421b4b076614655c7504300ccc06c2c
Galladite <galladite@yandex.com>
2023-06-05 19:04:56 +0100

    The thing builds and seems to work with .otf et al., but I'd like to investigate that plumerai bug before the next release.

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #define xstr(s) str(s)
 #define str(s) #s
 
-#define VER_NUMBER 20230417-en
+#define VER_NUMBER 20230605-en
 #define ONS_VERSION xstr(VER_NUMBER)
-#define ONS_CODENAME xstr(Fugu)
+#define ONS_CODENAME xstr(Donburi)
 #define NSC_VERSION 294
