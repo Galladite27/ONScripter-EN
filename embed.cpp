14d6a197cdb638b750c8c5167f207d867ba1d6f2
Sean McGovern <gseanmcg@gmail.com>
2023-11-21 23:36:25 -0500

    embed: convert to C++ / iostream

--- embed.cpp
+++ embed.cpp
@@ -1,7 +1,9 @@
 // A simple program to create resources.cpp
-// Usage: embed [input1 internalname1 input2...]
+// Usage: embed -o output [input1 internalname1 input2...]
 
-#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
 
 struct item {
     const char *file;
@@ -13,47 +15,73 @@ struct item {
 
 int main(int argc, char** argv)
 {
-    puts("// Generated file - do not edit");
-    puts("");
-    puts("#include <string.h>");
-    puts("#include \"resources.h\"");
-    for (int i = 1, j = 1; i < argc; i += 2, ++j) {
+    int result = 0;
+    std::ofstream output;
+
+    if (argc < 4) {
+        std::cerr << "ERROR : too few arguments!" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " -o output [input1 internalname1 input2...]" << std::endl;
+        result = 1;
+        exit(result);
+    }
+    output.open(argv[2]);
+    output << "// Generated file - do not edit\n";
+    output << "\n";
+    output << "#include <string.h>\n";
+    output << "#include \"resources.h\"" << std::endl;
+    for (int i = 3, j = 1; i < argc; i += 2, ++j) {
         FILE* f = fopen(argv[i], "rb");
-        if (f) {
+        if (!f) {
+            output.close();
+            std::cerr << "ERROR : could not open " << argv[i] << std::endl;
+            result = 1;
+            exit(result);
+        } else {
             int len = 0, c;
-            printf("\nstatic const unsigned char resource_%d_buffer[] = {", j);
+            char* formatted = new char[4];
+            output << "\nstatic const unsigned char resource_" << j << "_buffer[] = {";
             while ((c = getc(f)) != EOF) {
-                if (len) putchar(',');
+                if (len) output << ',';
 
-                if (len++ % 16 == 0) printf("\n\t");else putchar(' ');
+                if (len++ % 16 == 0) output << "\n\t"; else output << ' ';
 
-                printf("%3d", c);
+		snprintf(formatted, 4, "%3d", c);
+
+                output << formatted;
             }
+            delete formatted;
             fclose(f);
-            puts("\n};");
+            output << "\n};" << std::endl;
             names.next = new item(argv[i+1], len);
         }
     }
 
-    printf("\nstatic const InternalResource resource_list[] = {");
+    output << std::endl << "static const InternalResource resource_list[] = {";
     int i = 1;
+    const int len = 256;
     
     for (item *iptr = names.next; iptr != 0; ++i) {
-        printf("\n\t{ \"%s\", resource_%d_buffer, %d },",
+        char* str = new char[len];
+        snprintf(str, len, "\n\t{ \"%s\", resource_%d_buffer, %d },",
                iptr->file, i, iptr->len);
+        output << str;
+        delete str;
         item *tmp = iptr;
         iptr = iptr->next;
         delete tmp;
     }
     names.next = 0;
 
-    puts("\n\t{ 0, 0, 0 }\n};");
-    puts("");
-    puts("const InternalResource* getResource(const char* filename)");
-    puts("{");
-    puts("\tfor (const InternalResource* rv = resource_list; rv->buffer; ++rv) {");
-    puts("\t\tif (strcmp(rv->filename, filename) == 0) return rv;");
-    puts("\t}");
-    puts("\treturn NULL;");
-    puts("}");
+    output << "\n\t{ 0, 0, 0 }\n};\n";
+    output << "\nconst InternalResource* getResource(const char* filename)";
+    output << "\n{";
+    output << "\n\tfor (const InternalResource* rv = resource_list; rv->buffer; ++rv) {";
+    output << "\n\t\tif (strcmp(rv->filename, filename) == 0) return rv;";
+    output << "\n\t}";
+    output << "\n\treturn NULL;";
+    output << "\n}" << std::endl;
+
+    output.close();
+
+    return result;
 }
