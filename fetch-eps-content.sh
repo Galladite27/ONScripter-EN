22c7350a21b8ebc14c1d39753cd58461a3fcd26a
Galladite <galladite@yandex.com>
2024-10-07 16:07:35 +0100

    fetch-eps-content.sh

--- fetch-eps-content.sh
+++ fetch-eps-content.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if git clone https://github.com/Galladite27/ONScripter-EN-EPS eps ; then
-    echo "EPS content successfully downloaded. Please read eps/README.md and eps/BuildInstructions for usage information."
+if git clone https://github.com/Galladite27/ONScripter-EN-EPS eps >/dev/null 2>&1 ; then
+    echo "EPS content successfully downloaded. Please read eps/README.md and eps/BuildInstructions.md for usage information."
 else
     echo "EPS content download FAILED."
 fi
