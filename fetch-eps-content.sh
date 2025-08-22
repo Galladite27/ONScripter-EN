932cf17528a753fb20e91e3306dbede56650325b
THMprod <thmprod@gmail.com>
2025-08-22 21:16:46 +0200

    [EPS] - Add some option to download less data and save bandwidth

--- fetch-eps-content.sh
+++ fetch-eps-content.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if git clone https://github.com/Galladite27/ONScripter-EN-EPS eps >/dev/null 2>&1 ; then
+if git clone --depth 1 --branch master https://github.com/Galladite27/ONScripter-EN-EPS eps >/dev/null 2>&1 ; then
     echo "EPS content successfully downloaded. Please read eps/README.md and eps/BuildInstructions.md for usage information."
 else
     echo "EPS content download FAILED."
