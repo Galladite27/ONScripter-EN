9519b78c09bed3b0636efa4e7c3a0c3b08049490
Joshua T. Fisher <playmer@gmail.com>
2025-12-04 01:31:30 -0800

    Split off the png stuff as well.

--- tools/conv_shared.h
+++ tools/conv_shared.h
@@ -13,7 +13,6 @@ extern unsigned char *restored_buffer;
 extern size_t restored_length;
 #define INPUT_BUFFER_SIZE       4096
 
-
 void rescaleImage( unsigned char *original_buffer, int width, int height, int byte_per_pixel,
                    bool src_pad_flag, bool dst_pad_flag, bool palette_flag, int num_of_cells );
 
@@ -21,5 +20,11 @@ size_t rescaleJPEGWrite( unsigned int width, unsigned int height, int byte_per_p
                          int quality, bool bmp2jpeg_flag, int num_of_cells );
 size_t rescaleJPEG( unsigned char *original_buffer, size_t length,
                     unsigned char **rescaled_buffer, int quality, int num_of_cells=1 );
+
+size_t rescalePNGWrite( unsigned int width, unsigned int height, int byte_per_pixel, unsigned char **rescaled_buffer,
+                        void* /*png_structp*/ png_ptr, void* /*png_infop*/ info_ptr, bool palette_flag, int num_of_cells );
+
+size_t rescalePNG( unsigned char *original_buffer, size_t length,
+                    unsigned char **rescaled_buffer, int num_of_cells=1 );
 
 #endif
